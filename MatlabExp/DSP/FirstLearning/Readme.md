f22d850609832ca35e73448d24c00d9fe3057407
pacria <zhanghaobo@csu.edu.cn>
2020-07-06 12:07:37 +0800

    In Maintaince

---
+++ MatlabExp/DSP/FirstLearning/Readme.md
@@ -0,0 +1,1 @@
+When you first learn Matlab...(In maintenance)
---
+++ MatlabExp/DSP/FirstLearning/func_demo.m
@@ -0,0 +1,13 @@
+% function [return1, return2, ...]=func_name(arg1, arg2, ...)
+%   execulation
+% end
+
+function [s, p] = func_demo(r)
+s = pi*r*r;
+p = 2*pi*r;
+end
+
+% lambda func
+
+%func_handle = @(arg1, arg2) expression
+
